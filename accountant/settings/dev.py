8dec0834c163dd46dc674056feb1248f8a5dadd5
jjpsos <jjpsos@videotron.ca>
2017-01-30 16:23:16 -0500

    second commit

---
+++ accountant/settings/dev.py
@@ -0,0 +1,71 @@
+import os
+
+from .common import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'accountant',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+# Debug toolbar
+
+# INSTALLED_APPS += (
+#    'debug_toolbar.apps.DebugToolbarConfig',
+# )
+
+# DEBUG_TOOLBAR_CONFIG = {
+#    'JQUERY_URL': '',
+# }
+
+
+# Emailing
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "propagate": True,
+            "level": "WARNING",
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+        "django.db.backends": {
+            "handlers": ["console"],
+            "propagate": False,
+            "level": "WARNING",
+        },
+
+        "apps": {
+            "handlers": ["console"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+    }
+}
